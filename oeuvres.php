4b19e4c5624826cfb7d86061927f8605e99c7038
youkankroo <yourikancel.paris@gmail.com>
2024-07-21 09:28:09 +0200

    tableau oeuvre en Cours

---
+++ oeuvres.php
@@ -0,0 +1,21 @@
+<?php  
+
+$oeuvres[1] = 'dodomu'; 
+$oeuvres[2] = 'AASHAAHEEN BAADAL';
+$oeuvres[3] = 'NIGHTLIFE TRAFFIC';
+$oeuvres[4] = 'LE REFUGE DE L HAVRE'; 
+$oeuvres[5] = 'RED WASHOVER';
+$oeuvres[6] = 'CHROMATICS';
+$oeuvres[7] = 'DIGITAL NEGATIVE';
+$oeuvres[8] = 'BLAST FROM THE PAST';
+$oeuvres[9] = 'HURRICANE';
+$oeuvres[10]='LA MARÉE ROUGE ';
+$oeuvres[11]= 'ASIMILACION' ;
+$oeuvres[12]= 'LA GALAXIA GIALLA';
+$oeuvres[13]='PUFFY AMALGAMATE';
+$oeuvres[14]='MIRAGE';
+$oeuvres[15]='BLAUE GELBE MUSTER';
+?>
+<?php
+echo $oeuvres[1]; 
+?>
