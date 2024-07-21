a28f32ebc1c821b12ca2b95f08e419b72e9d415f
youkankroo <yourikancel.paris@gmail.com>
2024-07-21 19:23:19 +0200

    boucle oeuvre

--- oeuvres.php
+++ oeuvres.php
@@ -1,21 +1,33 @@
-<?php  
-
-$oeuvres[1] = $titre = 'DODOMU' ." ". $auteur = 'Mia Tozerski';
-$oeuvres[2] = $titre ='AASHAAHEEN BAADAL' ." ".$auteur = 'Anaisha Devi';
-$oeuvres[3] = $titre ='NIGHTLIFE TRAFFIC'." ".$auteur ='Andrew Forsythe';
-$oeuvres[4] = $titre = 'LE REFUGE DE L HAVRE'." ".$auteur = 'Simon Pelletier';
-$oeuvres[5] = $titre ='RED WASHOVER'." ".$auteur = 'Kit Van Der Borght';
-$oeuvres[6] = $titre ='CHROMATICS'." ". $auteur = 'Jean-Michel Delatronchette';
-$oeuvres[7] = $titre ='DIGITAL NEGATIVE'." ". $auteur ='Hamish McKee';
-$oeuvres[8] = $titre ='BLAST FROM THE PAST'. " ". $auteur = 'Juliette Baskerville' ;
-$oeuvres[9] = $titre ='HURRICANE'. " ". $auteur = 'Natalie Wellington';
-$oeuvres[10]= $titre ='LA MARÉE ROUGE '." ". $auteur = 'Martin Rodriguez';
-$oeuvres[11]= $titre ='ASIMILACION' ." ". $auteur = 'Angel Sanchez-Fernandez';
-$oeuvres[12]= $titre ='LA GALAXIA GIALLA'." ". $auteur = 'Eduardo Tancredi';
-$oeuvres[13]= $titre ='PUFFY AMALGAMATE'. " ". $auteur = 'Sandro De Blasi';
-$oeuvres[14]= $titre ='MIRAGE'. " " .$auteur = 'Stéphanie Kaiser';
-$oeuvres[15]= $titre ='BLAUE GELBE MUSTER'." ". $auteur = 'Adelheid Von Schreiber';
-?>
 <?php
-echo $oeuvres[1]." " . $oeuvres[2]." ". $oeuvres[3]." ".$oeuvres[4]." ". $oeuvres[5]." ". $oeuvres[6]." ". $oeuvres[7]." ". $oeuvres[8]." ". $oeuvres[9]." ". $oeuvres[10]." " . $oeuvres[11]." ". $oeuvres[12]." ".$oeuvres[13]." ". $oeuvres[14]." ". $oeuvres[15]; 
+// Définir les œuvres dans un tableau associatif
+$oeuvres = array(
+    1 => array('titre' => 'DODOMU', 'auteur' => 'Mia Tozerski'),
+    2 => array('titre' => 'AASHAAHEEN BAADAL', 'auteur' => 'Anaisha Devi'),
+    3 => array('titre' => 'NIGHTLIFE TRAFFIC', 'auteur' => 'Andrew Forsythe'),
+    4 => array('titre' => 'LE REFUGE DE L HAVRE', 'auteur' => 'Simon Pelletier'),
+    5 => array('titre' => 'RED WASHOVER', 'auteur' => 'Kit Van Der Borght'),
+    6 => array('titre' => 'CHROMATICS', 'auteur' => 'Jean-Michel Delatronchette'),
+    7 => array('titre' => 'DIGITAL NEGATIVE', 'auteur' => 'Hamish McKee'),
+    8 => array('titre' => 'BLAST FROM THE PAST', 'auteur' => 'Juliette Baskerville'),
+    9 => array('titre' => 'HURRICANE', 'auteur' => 'Natalie Wellington'),
+    10 => array('titre' => 'LA MARÉE ROUGE', 'auteur' => 'Martin Rodriguez'),
+    11 => array('titre' => 'ASIMILACION', 'auteur' => 'Angel Sanchez-Fernandez'),
+    12 => array('titre' => 'LA GALAXIA GIALLA', 'auteur' => 'Eduardo Tancredi'),
+    13 => array('titre' => 'PUFFY AMALGAMATE', 'auteur' => 'Sandro De Blasi'),
+    14 => array('titre' => 'MIRAGE', 'auteur' => 'Stéphanie Kaiser'),
+    15 => array('titre' => 'BLAUE GELBE MUSTER', 'auteur' => 'Adelheid Von Schreiber')
+);
+
+// Boucle pour générer le code HTML
+foreach ($oeuvres as $oeuvre) {
+    $titre = $oeuvre['titre'];
+    $auteur = $oeuvre['auteur'];
+
+    echo "
+    <div class='oeuvre'>
+        <h2>$titre</h2>
+         <p><strong>Auteur :</strong> $auteur</p>
+    </div>
+    ";
+}
 ?>
