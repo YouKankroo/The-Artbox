8ce5ee6f3b5d5a851696de60ed8537a4e24ea657
youkankroo <yourikancel.paris@gmail.com>
2024-07-21 18:50:43 +0200

    ?

--- oeuvres.php
+++ oeuvres.php
@@ -1,21 +1,21 @@
 <?php  
 
-$oeuvres[1] = 'dodomu'; 
-$oeuvres[2] = 'AASHAAHEEN BAADAL';
-$oeuvres[3] = 'NIGHTLIFE TRAFFIC';
-$oeuvres[4] = 'LE REFUGE DE L HAVRE'; 
-$oeuvres[5] = 'RED WASHOVER';
-$oeuvres[6] = 'CHROMATICS';
-$oeuvres[7] = 'DIGITAL NEGATIVE';
-$oeuvres[8] = 'BLAST FROM THE PAST';
-$oeuvres[9] = 'HURRICANE';
-$oeuvres[10]='LA MARÉE ROUGE ';
-$oeuvres[11]= 'ASIMILACION' ;
-$oeuvres[12]= 'LA GALAXIA GIALLA';
-$oeuvres[13]='PUFFY AMALGAMATE';
-$oeuvres[14]='MIRAGE';
-$oeuvres[15]='BLAUE GELBE MUSTER';
+$oeuvres[1] = $titre = 'DODOMU' ." ". $auteur = 'Mia Tozerski';
+$oeuvres[2] = $titre ='AASHAAHEEN BAADAL' ." ".$auteur = 'Anaisha Devi';
+$oeuvres[3] = $titre ='NIGHTLIFE TRAFFIC'." ".$auteur ='Andrew Forsythe';
+$oeuvres[4] = $titre = 'LE REFUGE DE L HAVRE'." ".$auteur = 'Simon Pelletier';
+$oeuvres[5] = $titre ='RED WASHOVER'." ".$auteur = 'Kit Van Der Borght';
+$oeuvres[6] = $titre ='CHROMATICS'." ". $auteur = 'Jean-Michel Delatronchette';
+$oeuvres[7] = $titre ='DIGITAL NEGATIVE'." ". $auteur ='Hamish McKee';
+$oeuvres[8] = $titre ='BLAST FROM THE PAST'. " ". $auteur = 'Juliette Baskerville' ;
+$oeuvres[9] = $titre ='HURRICANE'. " ". $auteur = 'Natalie Wellington';
+$oeuvres[10]= $titre ='LA MARÉE ROUGE '." ". $auteur = 'Martin Rodriguez';
+$oeuvres[11]= $titre ='ASIMILACION' ." ". $auteur = 'Angel Sanchez-Fernandez';
+$oeuvres[12]= $titre ='LA GALAXIA GIALLA'." ". $auteur = 'Eduardo Tancredi';
+$oeuvres[13]= $titre ='PUFFY AMALGAMATE'. " ". $auteur = 'Sandro De Blasi';
+$oeuvres[14]= $titre ='MIRAGE'. " " .$auteur = 'Stéphanie Kaiser';
+$oeuvres[15]= $titre ='BLAUE GELBE MUSTER'." ". $auteur = 'Adelheid Von Schreiber';
 ?>
 <?php
-echo $oeuvres[1]; 
+echo $oeuvres[1]." " . $oeuvres[2]." ". $oeuvres[3]." ".$oeuvres[4]." ". $oeuvres[5]." ". $oeuvres[6]." ". $oeuvres[7]." ". $oeuvres[8]." ". $oeuvres[9]." ". $oeuvres[10]." " . $oeuvres[11]." ". $oeuvres[12]." ".$oeuvres[13]." ". $oeuvres[14]." ". $oeuvres[15]; 
 ?>
