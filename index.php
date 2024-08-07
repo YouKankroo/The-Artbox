d730509a74af92c4a25083793cb7527e4aeddcfb
youkankroo <yourikancel.paris@gmail.com>
2024-08-07 14:28:16 +0200

    TEST

--- index.php
+++ index.php
@@ -18,21 +18,21 @@
         <?php
 // Définir les données des œuvres dans un tableau associatif
 $oeuvres_data = [
-    ['image' => 'img/clark-van-der-beken.png', 'titre' => 'DODOMU', 'auteur' => 'Mia Tozerski', 'link' => 'oeuvre-1.php'],
-    ['image' => 'img/dan-cristian-padure.png', 'titre' => 'AASHAAHEEN BAADAL', 'auteur' => 'Anaisha Devi', 'link' => 'oeuvre-2.php'],
-    ['image' => 'img/fly-d.png', 'titre' => 'NIGHTLIFE TRAFFIC', 'auteur' => 'Andrew Forsythe', 'link' => 'oeuvre-3.php'],
-    ['image' => 'img/jazmin-quaynor.png', 'titre' => 'LE REFUGE DE L HAVRE', 'auteur' => 'Simon Pelletier', 'link' => 'oeuvre-4.php'],
-    ['image' => 'img/orfeas-green.png', 'titre' => 'RED WASHOVER', 'auteur' => 'Kit Van Der Borght', 'link' => 'oeuvre-5.php'],
-    ['image' => 'img/pawel-czerwinski-2.png', 'titre' => 'CHROMATICS', 'auteur' => 'Jean-Michel Delatronchette', 'link' => 'oeuvre-6.php'],
-    ['image' => 'img/pawel-czerwinski-3.png', 'titre' => 'DIGITAL NEGATIVE', 'auteur' => 'Hamish McKee', 'link' => 'oeuvre-7.php'],
-    ['image' => 'img/pawel-czerwinski.png', 'titre' => 'BLAST FROM THE PAST', 'auteur' => 'Juliette Baskerville', 'link' => 'oeuvre-8.php'],
-    ['image' => 'img/steve-johnson-2.png', 'titre' => 'HURRICANE', 'auteur' => 'Natalie Wellington', 'link' => 'oeuvre-9.php'],
-    ['image' => 'img/steve-johnson-3.png', 'titre' => 'LA MARÉE ROUGE', 'auteur' => 'Martin Rodriguez', 'link' => 'oeuvre-10.php'],
-    ['image' => 'img/steve-johnson-4.png', 'titre' => 'ASIMILACION', 'auteur' => 'Angel Sanchez-Fernandez', 'link' => 'oeuvre-11.php'],
-    ['image' => 'img/steve-johnson-5.png', 'titre' => 'LA GALAXIA GIALLA', 'auteur' => 'Eduardo Tancredi', 'link' => 'oeuvre-12.php'],
-    ['image' => 'img/steve-johnson-6.png', 'titre' => 'PUFFY AMALGAMATE', 'auteur' => 'Sandro De Blasi', 'link' => 'oeuvre-13.php'],
-    ['image' => 'img/steve-johnson.png', 'titre' => 'MIRAGE', 'auteur' => 'Stéphanie Kaiser', 'link' => 'oeuvre-14.php'],
-    ['image' => 'img/victor-grabarczyk.png', 'titre' => 'BLAUE GELBE MUSTER', 'auteur' => 'Adelheid Von Schreiber', 'link' => 'oeuvre-15.php'],
+    ['image' => 'img/clark-van-der-beken.png', 'titre' => 'DODOMU', 'auteur' => 'Mia Tozerski', 'id' => '1'],
+    ['image' => 'img/dan-cristian-padure.png', 'titre' => 'AASHAAHEEN BAADAL', 'auteur' => 'Anaisha Devi', 'id' => '2'],
+    ['image' => 'img/fly-d.png', 'titre' => 'NIGHTLIFE TRAFFIC', 'auteur' => 'Andrew Forsythe', 'id' => '3'],
+    ['image' => 'img/jazmin-quaynor.png', 'titre' => 'LE REFUGE DE L HAVRE', 'auteur' => 'Simon Pelletier', 'id' => '4'],
+    ['image' => 'img/orfeas-green.png', 'titre' => 'RED WASHOVER', 'auteur' => 'Kit Van Der Borght', 'id' => '5'],
+    ['image' => 'img/pawel-czerwinski-2.png', 'titre' => 'CHROMATICS', 'auteur' => 'Jean-Michel Delatronchette', 'id' => '6'],
+    ['image' => 'img/pawel-czerwinski-3.png', 'titre' => 'DIGITAL NEGATIVE', 'auteur' => 'Hamish McKee', 'id' => '7'],
+    ['image' => 'img/pawel-czerwinski.png', 'titre' => 'BLAST FROM THE PAST', 'auteur' => 'Juliette Baskerville', 'id' => '8'],
+    ['image' => 'img/steve-johnson-2.png', 'titre' => 'HURRICANE', 'auteur' => 'Natalie Wellington', 'id' => '9'],
+    ['image' => 'img/steve-johnson-3.png', 'titre' => 'LA MARÉE ROUGE', 'auteur' => 'Martin Rodriguez', 'id' => '10'],
+    ['image' => 'img/steve-johnson-4.png', 'titre' => 'ASIMILACION', 'auteur' => 'Angel Sanchez-Fernandez', 'id' => '11'],
+    ['image' => 'img/steve-johnson-5.png', 'titre' => 'LA GALAXIA GIALLA', 'auteur' => 'Eduardo Tancredi', 'id' => '12'],
+    ['image' => 'img/steve-johnson-6.png', 'titre' => 'PUFFY AMALGAMATE', 'auteur' => 'Sandro De Blasi', 'id' => '13'],
+    ['image' => 'img/steve-johnson.png', 'titre' => 'MIRAGE', 'auteur' => 'Stéphanie Kaiser', 'id' => '14'],
+    ['image' => 'img/victor-grabarczyk.png', 'titre' => 'BLAUE GELBE MUSTER', 'auteur' => 'Adelheid Von Schreiber', 'id' => '15'],
 ];
 
 // Initialiser le tableau des œuvres
@@ -48,13 +48,13 @@ foreach ($oeuvres as $oeuvre) {
     $titre = $oeuvre['titre'];
     $auteur = $oeuvre['auteur'];
     $image = $oeuvre['image'];
-    $link = $oeuvre['link'];
+    $id = $oeuvre['id'];
 
     // Afficher ou utiliser les données de l'œuvre
-    echo "<a href='$link' class='oeuvre'>";
+    echo "<a href='$id' class='oeuvre'>";
     echo "<img src='$image' alt='$titre'>";
-    echo "<h2>$titre</h2>";
-    echo "<p>$auteur</p>";
+    echo "<h1>$titre</h1>";
+    echo "<p class='description'>$auteur</p>";
     echo "</a>";
 }
 ?>
--- oeuvres.php
+++ oeuvres.php
@@ -1,24 +1,22 @@
 <?php  
-$oeuvres = [];
-$oeuvres[1] = $image ='img/clark-van-der-beken.png'." ".$titre='DODOMU' ." ". $auteur = 'Mia Tozerski';
-$oeuvres[2] = $image = 'img/dan-cristian-padure.png'." ".$titre ='AASHAAHEEN BAADAL' ." ".$auteur = 'Anaisha Devi';
-$oeuvres[3] = $image = 'img/fly-d.png'." ".$titre ='NIGHTLIFE TRAFFIC'." ".$auteur ='Andrew Forsythe';
-$oeuvres[4] = $image ='img/jazmin-quaynor.png'." ".$titre = 'LE REFUGE DE L HAVRE'." ".$auteur = 'Simon Pelletier';
-$oeuvres[5] = $image ='img/orfeas-green.png'." ".$titre ='RED WASHOVER'." ".$auteur = 'Kit Van Der Borght';
-$oeuvres[6] = $image = 'img/pawel-czerwinski-2.png'." ".$titre ='CHROMATICS'." ". $auteur = 'Jean-Michel Delatronchette';
-$oeuvres[7] = $image ='img/pawel-czerwinski-3.png'." ".$titre ='DIGITAL NEGATIVE'." ". $auteur ='Hamish McKee';
-$oeuvres[8] = $image ='img/pawel-czerwinski.png'." ".$titre ='BLAST FROM THE PAST'. " ". $auteur = 'Juliette Baskerville' ;
-$oeuvres[9] = $image='img/steve-johnson-2.png'."". $titre ='HURRICANE'. " ". $auteur = 'Natalie Wellington';
-$oeuvres[10]= $image='img/steve-johnson-3.png'."". $titre ='LA MARÉE ROUGE '." ". $auteur = 'Martin Rodriguez';
-$oeuvres[11]= $image='img/steve-johnson-4.png'."". $titre ='ASIMILACION' ." ". $auteur = 'Angel Sanchez-Fernandez';
-$oeuvres[12]= $image='img/steve-johnson-5.png'."". $titre ='LA GALAXIA GIALLA'." ". $auteur = 'Eduardo Tancredi';
-$oeuvres[13]= $image='img/steve-johnson-6.png'."". $titre ='PUFFY AMALGAMATE'. " ". $auteur = 'Sandro De Blasi';
-$oeuvres[14]= $image='img/steve-johnson.png'."". $titre ='MIRAGE'. " " .$auteur = 'Stéphanie Kaiser';
-$oeuvres[15]= $image='img/victor-grabarczyk.png'."". $titre ='BLAUE GELBE MUSTER'." ". $auteur = 'Adelheid Von Schreiber';
+$oeuvres_data = [
+1=> ['image' => 'img/clark-van-der-beken.png', 'titre' => 'DODOMU', 'auteur' => 'Mia Tozerski', 'id' => '1'],
+2=> ['image' => 'img/dan-cristian-padure.png', 'titre' => 'AASHAAHEEN BAADAL', 'auteur' => 'Anaisha Devi','id' => '2'],
+3=> ['image' => 'img/fly-d.png', 'titre' => 'NIGHTLIFE TRAFFIC', 'auteur' => 'Andrew Forsythe', 'id' => '3'],
+4=> ['image' => 'img/jazmin-quaynor.png', 'titre' => 'LE REFUGE DE L HAVRE', 'auteur' => 'Simon Pelletier', 'id' => '4'],
+5=> ['image' => 'img/orfeas-green.png', 'titre' => 'RED WASHOVER', 'auteur' => 'Kit Van Der Borght', 'id' => '5'],
+6=> ['image' => 'img/pawel-czerwinski-2.png', 'titre' => 'CHROMATICS', 'auteur' => 'Jean-Michel Delatronchette', 'id' => '6'],
+7=> ['image' => 'img/pawel-czerwinski-3.png', 'titre' => 'DIGITAL NEGATIVE', 'auteur' => 'Hamish McKee', 'id' => '7'],
+8=> ['image' => 'img/pawel-czerwinski.png', 'titre' => 'BLAST FROM THE PAST', 'auteur' => 'Juliette Baskerville', 'id' => '8'],
+9=> ['image' => 'img/steve-johnson-2.png', 'titre' => 'HURRICANE', 'auteur' => 'Natalie Wellington', 'id' => '9'],
+10=>['image' => 'img/steve-johnson-3.png', 'titre' => 'LA MARÉE ROUGE', 'auteur' => 'Martin Rodriguez', 'id' => '10'],
+11=>['image' => 'img/steve-johnson-4.png', 'titre' => 'ASIMILACION', 'auteur' => 'Angel Sanchez-Fernandez', 'id' => '11'],
+12=>['image' => 'img/steve-johnson-5.png', 'titre' => 'LA GALAXIA GIALLA', 'auteur' => 'Eduardo Tancredi', 'id' => '12'],
+13=>['image' => 'img/steve-johnson-6.png', 'titre' => 'PUFFY AMALGAMATE', 'auteur' => 'Sandro De Blasi', 'id' => '13'],
+14=>['image' => 'img/steve-johnson.png', 'titre' => 'MIRAGE', 'auteur' => 'Stéphanie Kaiser', 'id' => '14'],
+15=>['image' => 'img/victor-grabarczyk.png', 'titre' => 'BLAUE GELBE MUSTER', 'auteur' => 'Adelheid Von Schreiber', 'id' => '15'],
+];
 
-foreach ($oeuvres_data as $index => $data) {
-    $oeuvres[$index + 1] = $data;
-}
 ?>
 
 
