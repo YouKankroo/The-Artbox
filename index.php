c3f21b0cca5ddad0300720d1a9991d372c4958d3
youkankroo <yourikancel.paris@gmail.com>
2024-08-09 22:01:08 +0200

    Fonctionnel

--- index.php
+++ index.php
@@ -18,21 +18,21 @@
         <?php
 // Définir les données des œuvres dans un tableau associatif
 $oeuvres_data = [
-    ['image' => 'img/clark-van-der-beken.png', 'titre' => 'DODOMU', 'auteur' => 'Mia Tozerski', 'id' => '1'],
-    ['image' => 'img/dan-cristian-padure.png', 'titre' => 'AASHAAHEEN BAADAL', 'auteur' => 'Anaisha Devi', 'id' => '2'],
-    ['image' => 'img/fly-d.png', 'titre' => 'NIGHTLIFE TRAFFIC', 'auteur' => 'Andrew Forsythe', 'id' => '3'],
-    ['image' => 'img/jazmin-quaynor.png', 'titre' => 'LE REFUGE DE L HAVRE', 'auteur' => 'Simon Pelletier', 'id' => '4'],
-    ['image' => 'img/orfeas-green.png', 'titre' => 'RED WASHOVER', 'auteur' => 'Kit Van Der Borght', 'id' => '5'],
-    ['image' => 'img/pawel-czerwinski-2.png', 'titre' => 'CHROMATICS', 'auteur' => 'Jean-Michel Delatronchette', 'id' => '6'],
-    ['image' => 'img/pawel-czerwinski-3.png', 'titre' => 'DIGITAL NEGATIVE', 'auteur' => 'Hamish McKee', 'id' => '7'],
-    ['image' => 'img/pawel-czerwinski.png', 'titre' => 'BLAST FROM THE PAST', 'auteur' => 'Juliette Baskerville', 'id' => '8'],
-    ['image' => 'img/steve-johnson-2.png', 'titre' => 'HURRICANE', 'auteur' => 'Natalie Wellington', 'id' => '9'],
-    ['image' => 'img/steve-johnson-3.png', 'titre' => 'LA MARÉE ROUGE', 'auteur' => 'Martin Rodriguez', 'id' => '10'],
-    ['image' => 'img/steve-johnson-4.png', 'titre' => 'ASIMILACION', 'auteur' => 'Angel Sanchez-Fernandez', 'id' => '11'],
-    ['image' => 'img/steve-johnson-5.png', 'titre' => 'LA GALAXIA GIALLA', 'auteur' => 'Eduardo Tancredi', 'id' => '12'],
-    ['image' => 'img/steve-johnson-6.png', 'titre' => 'PUFFY AMALGAMATE', 'auteur' => 'Sandro De Blasi', 'id' => '13'],
-    ['image' => 'img/steve-johnson.png', 'titre' => 'MIRAGE', 'auteur' => 'Stéphanie Kaiser', 'id' => '14'],
-    ['image' => 'img/victor-grabarczyk.png', 'titre' => 'BLAUE GELBE MUSTER', 'auteur' => 'Adelheid Von Schreiber', 'id' => '15'],
+    ['image' => 'img/clark-van-der-beken.png', 'titre' => 'DODOMU', 'auteur' => 'Mia Tozerski', 'id' => '1','link'=>'oeuvre.php?id=1'],
+    ['image' => 'img/pawel-czerwinski-3.png', 'titre' => 'AASHAAHEEN BAADAL', 'auteur' => 'Anaisha Devi', 'id' => '2'],
+    ['image' => 'img/dan-cristian-padure.png', 'titre' => 'NIGHTLIFE TRAFFIC', 'auteur' => 'Andrew Forsythe', 'id' => '3'],
+    ['image' => 'img/steve-johnson-5.png', 'titre' => 'LE REFUGE DE L HAVRE', 'auteur' => 'Simon Pelletier', 'id' => '4'],
+    ['image' => 'img/steve-johnson.png', 'titre' => 'RED WASHOVER', 'auteur' => 'Kit Van Der Borght', 'id' => '5'],
+    ['image' => 'img/pawel-czerwinski.png', 'titre' => 'CHROMATICS', 'auteur' => 'Jean-Michel Delatronchette', 'id' => '6'],
+    ['image' => 'img/jazmin-quaynor.png', 'titre' => 'DIGITAL NEGATIVE', 'auteur' => 'Hamish McKee', 'id' => '7'],
+    ['image' => 'img/steve-johnson-6.png', 'titre' => 'BLAST FROM THE PAST', 'auteur' => 'Juliette Baskerville', 'id' => '8'],
+    ['image' => 'img/victor-grabarczyk.png', 'titre' => 'HURRICANE', 'auteur' => 'Natalie Wellington', 'id' => '9'],
+    ['image' => 'img/pawel-czerwinski-2.png', 'titre' => 'LA MARÉE ROUGE', 'auteur' => 'Martin Rodriguez', 'id' => '10'],
+    ['image' => 'img/steve-johnson-2.png', 'titre' => 'ASIMILACION', 'auteur' => 'Angel Sanchez-Fernandez', 'id' => '11'],
+    ['image' => 'img/fly-d.png', 'titre' => 'LA GALAXIA GIALLA', 'auteur' => 'Eduardo Tancredi', 'id' => '12'],
+    ['image' => 'img/orfeas-green.png', 'titre' => 'PUFFY AMALGAMATE', 'auteur' => 'Sandro De Blasi', 'id' => '13'],
+    ['image' => 'img/steve-johnson-4.png', 'titre' => 'MIRAGE', 'auteur' => 'Stéphanie Kaiser', 'id' => '14'],
+    ['image' => 'img/steve-johnson-3.png', 'titre' => 'BLAUE GELBE MUSTER', 'auteur' => 'Adelheid Von Schreiber', 'id' => '15'],
 ];
 
 // Initialiser le tableau des œuvres
@@ -48,10 +48,10 @@ foreach ($oeuvres as $oeuvre) {
     $titre = $oeuvre['titre'];
     $auteur = $oeuvre['auteur'];
     $image = $oeuvre['image'];
-    $id = $oeuvre['id'];
-
+    
+     
     // Afficher ou utiliser les données de l'œuvre
-    echo "<a href='$id' class='oeuvre'>";
+    echo "<a href='oeuvre.php?id=" . $oeuvre['id'] . "' class='oeuvre'>";
     echo "<img src='$image' alt='$titre'>";
     echo "<h1>$titre</h1>";
     echo "<p class='description'>$auteur</p>";
